f13f540a7ad1783ef2f4c9f6bccc52af01d79939
Henry Heng <zhenjing@MacBook-Pro.local>
2017-08-26 12:12:14 +0100

    Initial Commit

---
+++ JobIn24/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JobIn24
+//
+//  Created by Henry Heng on 8/26/17.
+//  Copyright © 2017 Jonin24 Official Team. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
